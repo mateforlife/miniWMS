90f77deb73bcee38d47d73653cbb3f4cd52de208
MATEFORLIFE <RODRIGUEZ.RUIZ.ALEX@GMAIL.com>
2018-07-04 15:57:29 -0400

    some functional features

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -3,8 +3,8 @@ class Scheduling < ApplicationRecord
   belongs_to :client
   belongs_to :operation
   belongs_to :door
-  validates_presence_of :client_id, :user_id, :operation_id, :observation, :pallets_qty, :date, :door_id
-  validate :past_date, on: %i[create update]
+  validates_presence_of :client_id, :user_id, :operation_id, :observation, :pallets_qty, :date, :door_id, on: :create
+  validate :past_date, on: %i[create]
 
   def past_date
     errors.add(:date, 'No es posible agendar al pasado') if date.to_s < Time.now.to_s
