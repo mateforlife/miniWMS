2fe3d8c51240965b073077fbe9a81b2b18056deb
MATEFORLIFE <RODRIGUEZ.RUIZ.ALEX@GMAIL.COM>
2018-07-10 19:19:02 -0400

    to update layers

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   has_many :locations
   has_many :pallets
 
-  validates_presence_of :client_id, :code, :description, :client_code, :innerpack, :ean13, :dun14, :aux_code
-  validates_uniqueness_of :code, :description, :client_code, :ean13
+  #validates_presence_of :client_id, :code, :description, :client_code, :innerpack, :ean13, :dun14, :aux_code
+  #validates_uniqueness_of :code, :description, :client_code, :ean13
 
   def code_description
     "#{code} - #{description}"
